fa2172ddba5eee1ca7023efc9989097ada14c474
zielu652 <m.zielu2005@gmail.com>
2024-12-10 13:21:55 +0100

    Console Input interface and input in main

---
+++ src/GenerationAndIO/ConsoleImputable.java
@@ -0,0 +1,6 @@
+package GenerationAndIO;
+
+public interface ConsoleImputable {
+    public String GetDescription();
+    public void ShowMenu();
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import GenerationAndIO.ConsoleImputable;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<ConsoleImputable> listOfOptions = new ArrayList<ConsoleImputable>();
+        for(int i = 1; i <= listOfOptions.size(); i++)
+        {
+            System.out.println(i + ". " + listOfOptions.get(i).GetDescription());
+        }
+        Scanner scanner = new Scanner(System.in);
+        try
+        {
+            int choose =  scanner.nextInt();
+            listOfOptions.get(choose).ShowMenu();
+        }
+        catch(InputMismatchException e)
+        {
+            System.out.println("Podaj numer opcji.");
+        }
+        catch(IndexOutOfBoundsException e)
+        {
+            System.out.println("Nie ma takiej opcji.");
+        }
     }
 }
