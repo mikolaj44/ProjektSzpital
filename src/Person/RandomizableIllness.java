136c3fd3155adb378690035b6b9b7d450c5b7464
Devilian1993 <Maksioczko@gmail.com>
2024-12-10 16:15:36 +0100

    Zacommentowane RandomizableIllness, żeby dało się skompliować przez implementacji tej klasy

--- src/Person/RandomizableIllness.java
+++ src/Person/RandomizableIllness.java
@@ -1,6 +1,9 @@
 package Person;
 
-public class RandomizableIllness extends Illness{
+//Zacommentowałem to, bo przez to że nie są zaimplementowane wszystkie metody z Illness to się nie kompiluje
+//a nie chcę tego pisać na ten moment, bo nie jest to szczególnie potrzebne przy wpisywaniu pacjenta
+
+//public class RandomizableIllness extends Illness{
 
     // fields
 
@@ -9,4 +12,4 @@ public class RandomizableIllness extends Illness{
     // public void performAction(Patient p){}
 
     // public boolean hasBeenHealed(int currentDateDays){}; // override or don't
-}
+//}
